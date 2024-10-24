1d0658d9374f4d283b73541aaf8c58ea54722052
Kinny <kien.gkbeauty@gmail.com>
2024-10-24 17:36:20 +0700

    FEAT: add token in router post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { PostService } from './post.service';
 import { CrudController } from 'src/core/Base/crud.controller';
 import { CreatePostDto } from './dto/create-post.dto';
+import { JwtAuthGuard } from 'src/auth/guard/jwtGuard';
 @ApiTags('Post')
 @Controller('post')
+@ApiBearerAuth('JWT-auth')
+@UseGuards(JwtAuthGuard)
 export class PostController extends CrudController<PostService> {
   constructor(private readonly postService: PostService) {
     super(postService);
   }
 
-  @ApiBearerAuth()
   @Post()
   @ApiBody({ type: CreatePostDto })
   async create(@Body() body: any) {
